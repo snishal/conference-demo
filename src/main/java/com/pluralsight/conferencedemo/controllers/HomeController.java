5b62fc97222e7640b8d9e08612a289dfd7818aa1
sahilnishal <snishal33@gmail.com>
2021-01-28 18:55:12 +0530

    Home Controller

---
+++ src/main/java/com/pluralsight/conferencedemo/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package com.pluralsight.conferencedemo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+    @GetMapping
+    public String helloWorld(){
+        return "Hello World";
+    }
+}
